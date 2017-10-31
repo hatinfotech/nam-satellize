ef5846bf9ddadd4a84d128f63b1c50923ad41897
hoangtriet <hat.info.tech@gmail.com>
2017-10-31 10:54:45 +0700

    Skip compress checking

--- Src/Module/Backup/Controller/Client.php
+++ Src/Module/Backup/Controller/Client.php
@@ -336,7 +336,7 @@ class Backup_Controller_Client extends Controller {
                             unlink($listFile);
                         }
 
-                        if ($return > 0) {
+                        if (false && $return > 0) {//Skip compress checking
                             echo "Compress file fail\n";
                             unlink($backupFile);
                             throw new Exception_Business('Compress backup files was result failed');
