dd64aab5212756b3c55a67e8e565e77d4dd50c86
hoangtriet <hat.info.tech@gmail.com>
2017-11-03 00:54:07 +0700

    test upload by FTPClient lib with start position

--- Lib/FTPClient.php
+++ Lib/FTPClient.php
@@ -485,7 +485,7 @@ class FTPClient implements FTPClient_FTPClientInterface,
                 return false;
             }
             $count++;
-            if($count % 20) {
+            if($count % 1000) {
                 echo number_format($startPosition + $count * 10240) . " byte \n";
             }
         }
